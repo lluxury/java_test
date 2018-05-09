e8a1263b1398d41ff4a4e776c79ace631d3a79b2
yann <tmp4321@qq.com>
2018-05-09 19:22:53 +0800

    reflection base switch
    Signed-off-by: yann <tmp4321@qq.com>

---
+++ src/reflection/Service2.java
@@ -0,0 +1,8 @@
+package reflection;
+
+public class Service2 {
+
+    public void doService2(){
+        System.out.println("业务方法2");
+    }
+}
---
+++ src/reflection/Test.java
@@ -0,0 +1,8 @@
+package reflection;
+
+public class Test {
+
+    public static void main(String[] args) {
+        new Service1().doService1();
+    }
+}
