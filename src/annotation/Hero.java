2a28ec9afb88e2904aa1d0a681da385f66a10614
yann <tmp4321@qq.com>
2018-05-09 20:52:43 +0800

    annotation Override
    Signed-off-by: yann <tmp4321@qq.com>

---
+++ src/annotation/Hero.java
@@ -0,0 +1,14 @@
+package annotation;
+
+public class Hero {
+
+    String name;
+    @Override
+    public String toString(){
+        return name;
+    }
+//    @Override
+//    public String fromString(){
+//        return name;
+//    }
+}
