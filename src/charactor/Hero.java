061071c20d14f8ab74cdab87bb9f2a0751c27d16
yann <tmp4321@qq.com>
2018-05-09 15:33:28 +0800

    class object init
    Signed-off-by: yann <tmp4321@qq.com>

--- src/charactor/Hero.java
+++ src/charactor/Hero.java
@@ -1,10 +1,16 @@
 package charactor;
 
 public class Hero {
-    //增加id属性
-    public int id;
     public String name;
     public float hp;
     public int damage;
+    public int id;
+
+    static String copyright;
+
+    static {
+        System.out.println("初始化 copyright");
+        copyright = "版权由Riot Games公司所有";
+    }
 
 }
--- src/reflection/TestReflection.java
+++ src/reflection/TestReflection.java
@@ -10,8 +10,6 @@ public class TestReflection {
             Class pClass1=Class.forName(className);
             Class pClass2=Hero.class;
             Class pClass3=new Hero().getClass();
-            System.out.println(pClass1==pClass2);
-            System.out.println(pClass1==pClass3);
         } catch (ClassNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
