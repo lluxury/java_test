b2188e96c56060d94568548a55bc2aff8bcb68cc
yann <tmp4321@qq.com>
2018-05-09 21:00:38 +0800

    annotation SuppressWarnings
    Signed-off-by: yann <tmp4321@qq.com>

--- src/annotation/Hero.java
+++ src/annotation/Hero.java
@@ -1,15 +1,13 @@
 package annotation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hero {
-
     String name;
-
-    @Deprecated
-    public void hackMap(){
-
-    }
+    @SuppressWarnings({ "rawtypes", "unused" })
     public static void main(String[] args) {
-        new Hero().hackMap();
+        List heros = new ArrayList();
     }
 
 }
