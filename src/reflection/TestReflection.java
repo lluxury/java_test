7c26ed3aa59497d0b80107d83d0ae32594fdedbc
yann <tmp4321@qq.com>
2018-05-09 15:57:54 +0800

    reflection create object
    Signed-off-by: yann <tmp4321@qq.com>

--- src/reflection/TestReflection.java
+++ src/reflection/TestReflection.java
@@ -1,16 +1,26 @@
 package reflection;
-
+import java.lang.reflect.Constructor;
 import charactor.Hero;
-
 public class TestReflection {
 
     public static void main(String[] args) {
-        String className = "charactor.Hero";
+        //传统的使用new的方式创建对象
+        Hero h1 =new Hero();
+        h1.name = "teemo";
+        System.out.println(h1);
+
         try {
-            Class pClass1=Class.forName(className);
-            Class pClass2=Hero.class;
-            Class pClass3=new Hero().getClass();
-        } catch (ClassNotFoundException e) {
+            //使用反射的方式创建对象
+            String className = "charactor.Hero";
+            //类对象
+            Class pClass=Class.forName(className);
+            //构造器
+            Constructor c= pClass.getConstructor();
+            //通过构造器实例化
+            Hero h2= (Hero) c.newInstance();
+            h2.name="gareen";
+            System.out.println(h2);
+        } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
