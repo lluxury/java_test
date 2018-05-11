301b8fd7405b0b544cbc139df1b38076f2004a5c
yann <tmp4321@qq.com>
2018-05-11 11:12:16 +0800

    DBUtil with annotation
    Signed-off-by: yann <tmp4321@qq.com>

--- src/util/DBUtil.java
+++ src/util/DBUtil.java
@@ -1,17 +1,10 @@
 package util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import anno.JDBCConfig;
 
+@JDBCConfig(ip = "192.168.142.10", database = "test", encoding = "UTF-8", loginName = "root", password = "admin")
 public class DBUtil {
-    static String ip = "192.168.142.10";
-    static int port = 3306;
-    static String database = "test";
-    static String encoding = "UTF-8";
-    static String loginName = "root";
-    static String password = "admin";
-    static{
+    static {
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -19,11 +12,4 @@ public class DBUtil {
         }
     }
 
-    public static Connection getConnection() throws SQLException {
-        String url = String.format("jdbc:mysql://%s:%d/%s?characterEncoding=%s", ip, port, database, encoding);
-        return DriverManager.getConnection(url, loginName, password);
-    }
-    public static void main(String[] args) throws SQLException {
-        System.out.println(getConnection());
-    }
 }
