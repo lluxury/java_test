1587f6d2d3384b42c0bf0654d23c621b082a066b
yann <tmp4321@qq.com>
2018-05-11 11:39:56 +0800

    DBUtil with annotation JDBCConfig
    Signed-off-by: yann <tmp4321@qq.com>

--- src/util/DBUtil.java
+++ src/util/DBUtil.java
@@ -1,5 +1,9 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import anno.JDBCConfig;
 
 @JDBCConfig(ip = "192.168.142.10", database = "test", encoding = "UTF-8", loginName = "root", password = "admin")
@@ -12,4 +16,22 @@ public class DBUtil {
         }
     }
 
+    public static Connection getConnection() throws SQLException, NoSuchMethodException, SecurityException {
+        JDBCConfig config = DBUtil.class.getAnnotation(JDBCConfig.class);
+
+        String ip = config.ip();
+        int port = config.port();
+        String database = config.database();
+        String encoding = config.encoding();
+        String loginName = config.loginName();
+        String password = config.password();
+
+        String url = String.format("jdbc:mysql://%s:%d/%s?characterEncoding=%s", ip, port, database, encoding);
+        return DriverManager.getConnection(url, loginName, password);
+    }
+
+    public static void main(String[] args) throws NoSuchMethodException, SecurityException, SQLException {
+        Connection c = getConnection();
+        System.out.println(c);
+    }
 }
